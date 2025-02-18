31f7b4748f554142b62ababcdd8155f5333d7371
Ahmod Abdullah <ahmodabdullah876@gmail.com>
2025-02-18 10:43:31 +0800

    Basic Stuff

---
+++ C/hello_world.c
@@ -0,0 +1,13 @@
+// This is the first program for C. This is a single comment line.
+
+/*
+    And this one is a multi-line comment.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello World!");
+    return 0;
+}
