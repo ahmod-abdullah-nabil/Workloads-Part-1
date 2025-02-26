9b3e764af02dcd7e323173b8ff5cfb88295f05d3
Ahmod Abdullah <ahmodabdullah876@gmail.com>
2025-02-27 00:24:00 +0800

    Basic Tasks

--- C++/Task-1/task-6.cpp
+++ C++/Task-1/task-6.cpp
@@ -12,6 +12,39 @@ using namespace std;
 int main()
 {
     int score;
-    cout << "Enter the score: ";
+    char grade;
+    string Failed;
+
+    Failed = "You've Failed.";
+    cout << "Enter the score(1-100): ";
     cin >> score;
+
+    if (score < 0 || score > 100)
+    {
+        cout << "Invalid Score" << endl;
+        cout << "Enter the score(1-100): ";
+        cin >> score;
+    }
+
+    switch (score / 10)
+    {
+    case 10:
+    case 9:
+        grade = 'A';
+        break;
+    case8:
+        grade = 'B';
+        break;
+    case7:
+        grade = 'C';
+        break;
+    case6:
+        grade = 'D';
+        break;
+    default:
+        cout << Failed << endl;
+        grade = 'F';
+        break;
+    }
+    cout << "The grade is: " << grade << "." << endl;
 }
